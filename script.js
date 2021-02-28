d2700d031e428c377b09f7675b9d61893678446d
mateuszfranke <mateuszfranke@icloud.com>
2021-02-28 13:10:21 +0100

    swap players

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 'use strict';
 
 //selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const buttonRoll = document.querySelector('.btn--roll');
 const buttonNewGame = document.querySelector('.btn--new');
 const buttonHold = document.querySelector('.btn--hold');
-const player0Score = document.querySelector('#score--0');
-const player1Score = document.querySelector('#score--1');
-const player0CurrentScore = document.querySelector('#current--0');
-const player1CurrentScore = document.querySelector('#current--1');
+
+const player0ScoreElement = document.querySelector('#score--0');
+const player1ScoreElement = document.querySelector('#score--1');
+
+const player0CurrentScoreElement = document.querySelector('#current--0');
+const player1CurrentScoreElement = document.querySelector('#current--1');
+
 const diceImg = document.querySelector('.dice');
 //
-
+let activePlayer = 0;
+let currentScore = 0;
+const scores = [0, 0];
 
 initGame();
 
@@ -21,26 +29,48 @@ function roll() {
 function buttonRollListener() {
     const rolledNumber = roll();
     showRolledDice(rolledNumber);
-    player0CurrentScore.textContent = rolledNumber;
+    if (rolledNumber !== 1) {
+        addToCurrentScore(rolledNumber);
+    } else {
+        removeCurrentScoreAndSwitch();
+    }
+
 }
 
 function buttonHoldListener() {
-    
+
 }
 
-function showRolledDice(number){
+function showRolledDice(number) {
     console.log(number);
-    if(diceImg.classList.contains('hidden')) diceImg.classList.remove('hidden');
+    if (diceImg.classList.contains('hidden')) diceImg.classList.remove('hidden');
     diceImg.src = `dice-${number}.png`;
 }
 
 function initGame() {
     diceImg.classList.add('hidden');
-    player0Score.textContent = '0';
-    player1Score.textContent = '0';
-    player0CurrentScore.textContent = '0';
-    player1CurrentScore.textContent = '0';
+    player0ScoreElement.textContent = '0';
+    player1ScoreElement.textContent = '0';
+    player0CurrentScoreElement.textContent = '0';
+    player1CurrentScoreElement.textContent = '0';
 }
+
+function addToCurrentScore(rolledNumber) {
+
+    currentScore += rolledNumber;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+}
+
+function removeCurrentScoreAndSwitch() {
+
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
+
 
 // listeners
 buttonRoll.addEventListener('click', buttonRollListener);
